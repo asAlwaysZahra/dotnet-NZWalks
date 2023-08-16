ffab20c4c6381c6fd56d299789753ec66490df0a
Zahra Masoumi <zahraamasoumi@gmail.com>
2023-08-16 13:30:29 +0330

    create data context and related db sets

---
+++ NZWalks/NZWalks.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.API.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) {}
+
+        public int MyProperty { get; set; }
+    }
+}
