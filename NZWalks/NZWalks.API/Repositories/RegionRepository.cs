35c680c129ccefb35a6731fe4043bfdba8787377
Zahra Masoumi <zahraamasoumi@gmail.com>
2023-08-18 12:22:06 +0330

    create region repository interface and an sql implementation

---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly DataContext context;
+
+        public RegionRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Region>> GetAllAsync()
+        {
+            return await context.Regions.ToListAsync();
+        }
+
+        public async Task<Region?> GetByIdAsync(Guid id)
+        {
+            return await context.Regions.FindAsync(id);
+        }
+
+        public async Task<Region> CreateAsync(Region region)
+        {
+            await context.Regions.AddAsync(region);
+            await context.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region?> UpdateAsync(Guid id, Region region)
+        {
+            var existingRegion = await context.Regions.FirstOrDefaultAsync(r => r.Id == id);
+
+            if (existingRegion == null)
+            {
+                return null;
+            }
+
+            existingRegion.Code = region.Code;
+            existingRegion.Name = region.Name;
+            existingRegion.ImageUrl = region.ImageUrl;
+
+            await context.SaveChangesAsync();
+            return existingRegion;
+        }
+
+        public async Task<Region?> DeleteAsync(Guid id)
+        {
+            var existingRegion = await context.Regions.FirstOrDefaultAsync(r => r.Id == id);
+
+            if (existingRegion == null)
+            {
+                return null;
+            }
+
+            context.Regions.Remove(existingRegion);
+            await context.SaveChangesAsync();
+
+            return existingRegion;
+        }
+    }
+}
