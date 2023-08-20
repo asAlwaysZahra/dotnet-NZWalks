4a445ab1c41590862c0d81d6e68681a1710caefd
Zahra Masoumi <zahraamasoumi@gmail.com>
2023-08-20 17:56:25 +0330

    create dtos for walk and define mappings

--- NZWalks/NZWalks.API/Mappings/AutomapperProfiles.cs
+++ NZWalks/NZWalks.API/Mappings/AutomapperProfiles.cs
@@ -11,6 +11,8 @@ namespace NZWalks.API.Mappings
             CreateMap<Region, RegionDto>().ReverseMap();
             CreateMap<AddRegionDto, Region>();
             CreateMap<UpdateRegionDto, Region>();
+            CreateMap<AddWalkDto, Walk>();
+            CreateMap<Walk, WalkDto>();
         }
     }
 }
