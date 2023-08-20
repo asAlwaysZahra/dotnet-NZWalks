4f191c3ac3be1b5a940bf8087f81408492fa0ddf
Zahra Masoumi <zahraamasoumi@gmail.com>
2023-08-20 17:57:15 +0330

    create walk repository and its implementation

---
+++ NZWalks/NZWalks.API/Repositories/WalkRepository.cs
@@ -0,0 +1,22 @@
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class WalkRepository : IWalkRepository
+    {
+        private readonly DataContext context;
+
+        public WalkRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Walk> CreateAsync(Walk walk)
+        {
+            await context.Walks.AddAsync(walk);
+            await context.SaveChangesAsync();
+            return walk;
+        }
+    }
+}
