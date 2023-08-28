f0bf013386f71d427b7fa6970afb0ce35c1e3331
Zahra Masoumi <zahraamasoumi@gmail.com>
2023-08-28 22:09:31 +0330

    init ui project

---
+++ NZWalks/NZWalks.Web/Controllers/RegionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.Web.Controllers
+{
+    public class RegionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
